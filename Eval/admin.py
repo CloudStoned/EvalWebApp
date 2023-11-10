f0005fd41d77e9e0e7b027db71364fa4b5d165e5
CloudStone <92814911+CloudStoned@users.noreply.github.com>
2023-11-11 00:44:27 +0800

    Initial Commit

---
+++ Eval/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Author, Evaluation, Question, Choice
+
+class EvaluationInLine(admin.TabularInline):
+    model = Evaluation
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [EvaluationInLine]
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Question, QuestionAdmin)
---
+++ Eval/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Evaluation(models.Model):
+    eval_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    eval_name = models.CharField(max_length=100)
+    date_created = models.DateTimeField ("Eval Date Created")
+
+    def __str__(self):
+        return f"{self.eval_author} - {self.eval_name} - {self.date_created}"
+    
+class Question(models.Model):
+    eval_form = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.eval_form} - {self.question_text}"
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    answers = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.question} - {self.choice_text} - {self.answers}"
